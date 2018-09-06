c2b20e129477847451b1ec982e41420d7b5945ea
Andrej <andrej.prijic89@gmail.com>
2018-09-07 00:09:10 +1000

    Wavepoint now handles interaction

--- WavePoint.js
+++ WavePoint.js
@@ -4,16 +4,59 @@ function WavePoint(x, y, rad, seed) {
 	this.yOff = y;
 	this.rad = rad;
 	this.seed = seed;
+
+	this.defRad = rad; // default radius
+	this.hovRad = rad + 8; // expanded radius
+
+	this.hLink;
 	
+	/* Interaction methods */
+	this.hovered = function() {
+		let mDist = dist(mouse.x, mouse.y, this.x, this.y);
+		return mDist < this.rad;
+	}
+	
+	this.clicked = function() {
+		return this.hovered() && mouseDown;
+	}
+	
+	this.released = function() {
+		return this.hovered() && mouseUp;
+	}
+	
+	/* Main methods */
 	this.update = function(t) {
+
+		if (this.hovered()) {
+			if (this.rad < this.hovRad) { // inflate
+				this.rad = lerp(this.rad, this.hovRad, 0.11);
+			}
+		} else if (this.rad > this.defRad) { // deflate
+			this.rad = lerp(this.rad, this.defRad, 0.11);
+		}
+
+		// Update wave
 		this.y = this.yOff + Math.sin(this.seed) * 50;
 		this.seed += 0.5 * t;
 	}
 	
 	this.display = function() {
-		ctx.lineWidth = 3;
-		ctx.beginPath();
-		ctx.arc(this.x, this.y, this.rad, 0, 2*Math.PI);
-		ctx.stroke();
+		// Fill behaviour
+		if (this.hovered()) {
+			ctx.fillStyle = "#505050";
+		} else { ctx.fillStyle = "#282829"; } // dark grey
+
+		if (this.clicked()) {
+			ctx.fillStyle = "#707070";
+		}
+		  
+		if (this.released()) {
+			ctx.fillStyle = "#FFFFFF"; // white
+			window.location.assign(this.hLink); // hyperlink to page
+			print(this.hLink);
+		}
+
+		ctx.lineWidth = 4;
+		circle(this.x, this.y, this.rad, true);
 	}
 }
