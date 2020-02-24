5464135565da39dffd63b8d90d248e873fbd12ae
Andrej <andrej.prijic89@gmail.com>
2020-02-25 00:16:08 +1100

    Fixed text display, reordered links

--- wavePointsLoop.js
+++ wavePointsLoop.js
@@ -2,11 +2,18 @@ var mouseUp = false;
 var mouseDown = false;
 
 var points = [];
-var links = ["PerlinJelly.html", "Web Audio.html", "voiceField.html",
-	     "Wavefolding.html", "QuadraticBerzier.html", "DistanceFields.html"];
+
+var links = ["PerlinJelly.html", "Wavefolding.html", "QuadraticBerzier.html", 
+			 "DistanceFields.html", "voiceField.html", "Web Audio.html"];
+
+var titles = ["Perlin Jelly", "Wavefolding", "Quadratic Berzier",
+			  "Distance Fields", "Voice Field", "FM Mouse", "", "", "", "", "", ""];
+
+var helpText = "";
+
 var notes = [440.0, 493.88, 554.37, 587.33, 659.26, 739.99, 830.61, 880.0, 987.77, 1108.7, 1174.7, 1318.5];
 
-var resumed = false;
+var firstHover = false;
 
 function setup() {
     canvasSize(800, 500);
@@ -25,14 +32,16 @@ function setup() {
 }
 
 function draw() {
-	if (mouseDown && resumed) {
-		audioContext.resume(); // allows audio to play when autoplay disabled
-		resumed = true;
-	}
 	
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
 	ctx.strokeStyle = '#AAAAAA';
 	ctx.lineWidth = 1;
+	helpText = ""; // hide text by defualt
+	if (!audioEnabled && !firstHover) {
+		helpText = "Click anywhere to enable audio.";
+	}
+	
+	//rect(0, 0, 800, 500); // canvas border
 	
 	// Draw verticies
 	ctx.beginPath();
@@ -40,14 +49,18 @@ function draw() {
 		ctx.lineTo(points[i].x, points[i].y);
 	}
 	ctx.stroke();
-	
+
 	// move and draw points
 	for (i = 0; i < points.length; i++) {
 		points[i].update(dt);
 		points[i].display();
+
+		if (points[i].hovered()) {
+			helpText = titles[i];
+			firstHover = true;
+		}
 	}
-	
-	
+
 	if (!audioEnabled) {
 		ctx.lineWidth = 4;
 		let crossSize = 20;
@@ -55,13 +68,15 @@ function draw() {
 		line(750, 470, 770, 450);
 	}
 
+	drawText(helpText);
+
 	mouseUp = false; // reset
 }
 
 function mousePressed() {
 	mouseDown = true;
 	mouseUp = false;
-	
+
 	if (!audioEnabled) {
 		enableAudio();
 		audioEnabled = true;
@@ -71,4 +86,10 @@ function mousePressed() {
 function mouseReleased() {
 	mouseDown = false;
 	mouseUp = true;
+}
+
+function drawText(txt) {
+	ctx.fillStyle = "#909090";
+	ctx.font = "14px Segoe UI";
+	ctx.fillText(txt, 50, 470);
 }
